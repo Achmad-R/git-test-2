eb4fbbc575c827f69c0fdf31441d73c96a3fa476
AchmadR <achmad.ridho.st@gmail.com>
2022-05-16 17:05:17 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Coba Git Remote</title>
 </head>
 <body>
-   <h1>Hello World.!</h1> 
+   <h1>Hello World.!</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis et eveniet voluptates nam doloremque enim aspernatur libero ex optio aliquam? Aperiam dolore veritatis illum ratione iusto deserunt officia dolores labore.</p>
 </body>
 </html>
